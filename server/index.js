6cc4b335a607927750bdf3f89e60f6d9ebe32cd2
PDrago55 <drago_paolo@yahoo.ca>
2021-04-07 17:24:24 -0400

    server changes again

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.get("/ping", function (req, res) {
   return res.send("pong");
 });
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "server/build/index.html"));
 });
 app.listen(port);
 
