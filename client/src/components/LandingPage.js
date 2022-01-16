cf024e67e45bd71786f18bb3174c7365dff66c43
PDrago55 <drago_paolo@yahoo.ca>
2022-01-16 11:50:50 -0500

    [BUG FIX] removing bg

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./LandingPage.css";
 import ArrowUp from "./ArrowUp";
-import Emoji from "./Emoji";
+//import Emoji from "./Emoji";
 import About from "./About";
 import Languages from "./Languages";
 import Projects from "./Projects";
@@ -16,13 +16,13 @@ function LandingPage() {
       <div className="container">
         <div className="bg_lp">
           <div className="text-container">
-            <h1 className="intro">
+            {/* <h1 className="intro">
               Hi I'm Paolo!
               <Emoji symbol="👨‍💻" label="man technologist" />{" "}
             </h1>
             <div className="welcome">
               Welcome to my Website <Emoji symbol="🔥" label="fire" />
-            </div>
+            </div> */}
           </div>
         </div>
         <About />
