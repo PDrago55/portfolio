76885b916c748bc7a2230020bcb91698ba1c27ba
PDrago55 <drago_paolo@yahoo.ca>
2022-01-28 11:22:04 -0500

    :?

---
+++ client/build/precache-manifest.238e7878f0d18f0eb967df04111399ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a953c160f273eb281a5653066551bde9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6533173b3e254f1dc8a",
+    "url": "/static/css/main.20e6cfff.chunk.css"
+  },
+  {
+    "revision": "ad794256dd11ea932e1d",
+    "url": "/static/js/2.4ec9c047.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.4ec9c047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6533173b3e254f1dc8a",
+    "url": "/static/js/main.7a04831f.chunk.js"
+  },
+  {
+    "revision": "b417427353ba49748b8b",
+    "url": "/static/js/runtime-main.6736e556.js"
+  }
+]);
